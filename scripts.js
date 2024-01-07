2b33caf7abad68c259d87d6166e1f95f69f382a5
Emerson Rodrigues <emerson_18vg2008@hotmail.com>
2024-01-07 17:01:45 -0400

    update projects

--- scripts.js
+++ scripts.js
@@ -13,33 +13,33 @@ const projects = [
     },
      {
         title: 'Projeto 3',
-        description: 'Análise Descritiva para geração de insight',
+        description: 'Código-fonte de uma análise de dados com dados gerados por um arquivo em .CSV, usando python" "Este conjunto de dados inclui preços e variação percentual de preço para lã grossa, copra, algodão, lã fina, dura longa, madeira serrada dura, couro, madeira compensada, borracha, toras macias, madeira serrada macia e celulose de madeira.',
         status: 'Concluído',
-        link: 'Código-fonte de uma análise de dados com dados gerados por um arquivo em .CSV, usando python" "Este conjunto de dados inclui preços e variação percentual de preço para lã grossa, copra, algodão, lã fina, dura longa, madeira serrada dura, couro, madeira compensada, borracha, toras macias, madeira serrada macia e celulose de madeira.'
+        link: 'https://github.com/esrodrigues01/Data-Scientist/tree/main/Projeto%206%20-%20Analise%20de%20dados%20com%20SQL/src'
     },
      {
         title: 'Projeto 4',
-        description: 'Análise de dados com dados gerados por um banco de dados, usando python e SQL" "Dados de Pesquisas eleitorais compiladas pelo poder360.',
+        description: 'Os dados de eleições brasileiras de 2000 a 2022 em SQL incluem informações sobre os candidatos, partidos políticos, cargos disputados, resultados das eleições e informações geográficas, como municípios e estados.',
         status: 'Concluído',
-        link: 'https://github.com/esrodrigues01/Data-Scientist/tree/main/Projeto%202%20-%20Elei%C3%A7%C3%B5es%20brasileiras'
+        link: ''
     },
      {
         title: 'Projeto 5',
-        description: 'Análise de dados com dados gerados por um banco de dados, usando python e SQL" "Dados de Pesquisas eleitorais compiladas pelo poder360.',
+        description: 'A previsão de preços de ações é uma área de grande interesse para investidores e pesquisadores financeiros. A utilização de técnicas de aprendizado de máquina (Machine Learning) em Python tem se mostrado uma opção eficiente para a previsão de preços de ações. O primeiro passo na construção de um modelo de previsão de preços de ações é a coleta de dados históricos. O próximo passo é a seleção do algoritmo de Machine Learning a ser utilizado. Com o algoritmo escolhido, é realizado o treinamento do modelo, utilizando dados históricos para estimar os parâmetros do modelo.',
         status: 'Concluído',
-        link: 'https://github.com/esrodrigues01/Data-Scientist/tree/main/Projeto%202%20-%20Elei%C3%A7%C3%B5es%20brasileiras'
+        link: ''
     },
      {
         title: 'Projeto 6',
-        description: 'Análise de dados com dados gerados por um banco de dados, usando python e SQL" "Dados de Pesquisas eleitorais compiladas pelo poder360.',
+        description: 'A previsão de preços de ações é uma área de grande interesse para investidores e pesquisadores financeiros. A utilização de técnicas de aprendizado de máquina (Machine Learning) em Python tem se mostrado uma opção eficiente para a previsão de preços de ações. O primeiro passo na construção de um modelo de previsão de preços de ações é a coleta de dados históricos. O próximo passo é a seleção do algoritmo de Machine Learning a ser utilizado. Com o algoritmo escolhido, é realizado o treinamento do modelo, utilizando dados históricos para estimar os parâmetros do modelo.',
         status: 'Concluído',
-        link: 'https://github.com/esrodrigues01/Data-Scientist/tree/main/Projeto%202%20-%20Elei%C3%A7%C3%B5es%20brasileiras'
+        link: ''
     },
      {
         title: 'Projeto 7',
-        description: 'Análise de dados com dados gerados por um banco de dados, usando python e SQL" "Dados de Pesquisas eleitorais compiladas pelo poder360.',
+        description: '',
         status: 'Concluído',
-        link: 'https://github.com/esrodrigues01/Data-Scientist/tree/main/Projeto%202%20-%20Elei%C3%A7%C3%B5es%20brasileiras'
+        link: ''
     },
      {
         title: 'Projeto 8',
